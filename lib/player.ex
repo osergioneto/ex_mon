cc432063c97d30a6ddefd53bd2598e97eb467366
Sérgio Deusdedith de Araujo Neto <sergio.deusdedith@gmail.com>
2020-10-06 22:19:03 -0300

    feat: add Player struct

---
+++ lib/player.ex
@@ -0,0 +1,17 @@
+defmodule ExMon.Player do
+  @required_keys [:name, :move_rnd, :move_avg, :move_heal, :life]
+  @max_life 100
+
+  @enforce_keys @required_keys
+  defstruct @required_keys
+
+  def build(name, move_avg, move_heal, move_rnd) do
+    %ExMon.Player{
+      life: @max_life,
+      move_avg: move_avg,
+      move_heal: move_heal,
+      move_rnd: move_rnd,
+      name: name
+    }
+  end
+end
