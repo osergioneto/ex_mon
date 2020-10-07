a10a461d490ed78f09c4a5a4d544b93add87f3d4
Sérgio Deusdedith de Araujo Neto <sergio.deusdedith@gmail.com>
2020-10-06 23:45:49 -0300

    feat: add start_game function on main module

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,6 +1,14 @@
 defmodule ExMon do
-  alias ExMon.Player
+  alias ExMon.{Player, Game}
+
+  @computer_name "Robotinik"
   def create_player(name, move_avg, move_heal, move_rnd) do
     Player.build(name, move_avg, move_heal, move_rnd)
+  end
+
+  def start_game(player) do
+    @computer_name
+      |> ExMon.create_player(:punch, :cure, :kick)
+      |> Game.start(player)
   end
 end
