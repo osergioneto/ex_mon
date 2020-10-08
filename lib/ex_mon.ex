e1f0f3884a77ab8d2ccd3b8a9fe9f9b44e745f81
Sérgio Deusdedith de Araujo Neto <sergio.deusdedith@gmail.com>
2020-10-07 22:03:23 -0300

    feat: add function to attack opponent or cure

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,5 +1,7 @@
 defmodule ExMon do
   alias ExMon.{Player, Game}
+  alias ExMon.Game.{Status, Actions}
+  alias ExMon.Game.Actions.Attack
 
   @computer_name "Robotinik"
   def create_player(name, move_avg, move_heal, move_rnd) do
@@ -10,5 +12,21 @@ defmodule ExMon do
     @computer_name
       |> ExMon.create_player(:punch, :cure, :kick)
       |> Game.start(player)
+
+    Status.print_start_game()
+  end
+
+  def make_move(move) do
+    move
+    |> Actions.fetch_move()
+    |> do_move()
+  end
+
+  defp do_move({:error, move}), do: Status.print_wrong_move_message(move)
+  defp do_move({:ok, move}) do
+    case move do
+      :move_heal -> "Cura"
+      move -> Attack.attack_opponent(Game.turn(), move)
+    end
   end
 end
