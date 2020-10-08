8b1bca234b8099e39a71c4de700a45de2b61592d
Sérgio Deusdedith de Araujo Neto <sergio.deusdedith@gmail.com>
2020-10-08 00:34:38 -0300

    fix: use attack instead of attack_opponent

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -26,7 +26,7 @@ defmodule ExMon do
   defp do_move({:ok, move}) do
     case move do
       :move_heal -> "Cura"
-      move -> Attack.attack_opponent(Game.turn(), move)
+      move -> Actions.attack(move)
     end
   end
 end
